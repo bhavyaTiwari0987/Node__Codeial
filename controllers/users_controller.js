d9a6b2e05e401ad46d408eb11c65842c0b80cf59
Bhavya Tiwari <bhavyatiwari917@gmail.com>
2023-05-17 17:13:58 +0530

    Add Sign In code

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -13,6 +13,22 @@ module.exports.signIn = function (req, res){
         title: "Codeial | Sign In"
     })
 }
+//render the profile page
+module.exports.profile = async function (req, res){
+    if(req.cookies.user_id){
+        const currentUser = await User.findById(req.cookies.user_id);
+        if(currentUser){
+            return res.render('user_profile' , {
+                title: "Codeial | Profile-Page",
+                user:currentUser
+            })
+        }
+            return res.redirect('/users/sign-in');
+    }else {
+        return res.redirect('/users/sign-in');
+    }
+   
+}
 
 // get the sign up data
 module.exports.create= async function (req, res){
@@ -55,26 +71,9 @@ module.exports.createSession = async function (req, res){
             res.cookie('user_id' , currentUser.id);
             return res.redirect('/users/profile');
         }else {
-            console.log('nahi mila user');
             return res.redirect('back');
         }
     }catch(error){
         console.log('Error' , error);
-    }
-
-    
-    // if(user){
-    //     //handle passwords which doesn't match
-    //     if(user.password != req.body.password){
-    //         return res.redirect('back');
-    //     }
-    //     //handle session creation
-    //     res.cookie('user_id' , user.id);
-    //     return res.redirect('/users/profile');
-    // }else{
-    //     //handle user not found
-    //     return res.redirect('back');
-
-    // }
-   
+    }   
 }
