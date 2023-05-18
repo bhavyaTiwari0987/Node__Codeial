f2e40fa649f8e0591e3e705b0f501383a6e9930d
Bhavya Tiwari <bhavyatiwari917@gmail.com>
2023-05-18 14:49:46 +0530

    Add Authorization to the Create Post Form

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+
 const router = express.Router();
 
 const PostsController = require('../controllers/posts_controller');
 
-router.post('/create', PostsController.create);
+router.post('/create', passport.checkAuthentication, PostsController.create);
 
 module.exports = router;
